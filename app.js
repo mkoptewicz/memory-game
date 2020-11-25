96c3ae51e95a624d25295e5c4483d7d78ccec1f8
Michal Koptewicz <mkoptewicz@gmail.com>
2020-11-25 09:01:57 +0100

    create card array

---
+++ app.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "andy",
+      img: "src/andy.jpg",
+    },
+    {
+      name: "andy",
+      img: "src/andy.jpg",
+    },
+    {
+      name: "angela",
+      img: "src/angela.jpg",
+    },
+    {
+      name: "angela",
+      img: "src/angela.jpg",
+    },
+    {
+      name: "dwight",
+      img: "src/dwight.jpg",
+    },
+    {
+      name: "dwight",
+      img: "src/dwight.jpg",
+    },
+    {
+      name: "jim",
+      img: "src/jim.jpg",
+    },
+    {
+      name: "jim",
+      img: "src/jim.jpg",
+    },
+    {
+      name: "karen",
+      img: "src/karen.jpg",
+    },
+    {
+      name: "karen",
+      img: "src/karen.jpg",
+    },
+    {
+      name: "michael",
+      img: "src/michael.jpg",
+    },
+    {
+      name: "michael",
+      img: "src/michael.jpg",
+    },
+    {
+      name: "oscar",
+      img: "src/oscar.jpg",
+    },
+    {
+      name: "oscar",
+      img: "src/oscar.jpg",
+    },
+    {
+      name: "pam",
+      img: "src/pam.jpg",
+    },
+    {
+      name: "pam",
+      img: "src/pam.jpg",
+    },
+  ];
+});
